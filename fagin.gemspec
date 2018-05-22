5831f09d3c1f681c44d98690a9c6f1c061e98fb2
Miles Whittaker <mjwhitta@gmail.com>
2018-05-22 11:18:54 -0400

    Can now get the file containing the class

--- fagin.gemspec
+++ fagin.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
     s.name = "fagin"
-    s.version = "1.1.3"
+    s.version = "1.2.0"
     s.date = Time.new.strftime("%Y-%m-%d")
     s.summary = "Dynamically load children classes"
     s.description =
--- lib/fagin.rb
+++ lib/fagin.rb
@@ -1,7 +1,12 @@
 require "pathname"
 
 class Fagin
-    def self.find_children(parent, dir, recursive = false)
+    def self.find_children(
+        parent,
+        dir,
+        recursive = false,
+        withfile = false
+    )
         children = Hash.new
         path = Pathname.new(dir).expand_path
         return children if (!path.exist?)
@@ -21,7 +26,11 @@ class Fagin
                     child = clas.split("::").inject(Object) do |m, c|
                         m.const_get(c)
                     end
-                    children[clas] = child
+                    if (withfile)
+                        children[clas] = [child, file]
+                    else
+                        children[clas] = child
+                    end
                 rescue NameError
                     raise Error::UnknownChildClass.new(clas)
                 end
@@ -33,6 +42,14 @@ class Fagin
 
     def self.find_children_recursively(parent, dir)
         return find_children(parent, dir, true)
+    end
+
+    def self.find_children_with_file(parent, dir)
+        return find_children(parent, dir, false, true)
+    end
+
+    def self.find_children_with_file_recursively(parent, dir)
+        return find_children(parent, dir, true, true)
     end
 end
 
